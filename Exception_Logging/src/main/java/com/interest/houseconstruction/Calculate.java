126b245319e025b0056b1c65132280e24e1e5883
harsh <harshithauriti@gmail.com>
2020-02-26 22:02:09 +0530

    logging of construction cost and intrest

---
+++ Exception_Logging/src/main/java/com/interest/houseconstruction/Calculate.java
@@ -0,0 +1,8 @@
+package com.interest.houseconstruction;
+
+abstract class Calculate {
+	
+	abstract double totalamount(double principal,double rateofintrest,double time,int count);
+	
+
+}
---
+++ Exception_Logging/src/main/java/com/interest/houseconstruction/Standard.java
@@ -0,0 +1,8 @@
+package com.interest.houseconstruction;
+
+public class Standard extends Cost{
+
+	public void getRate(){  
+        rate=1200;              
+   }  
+}
